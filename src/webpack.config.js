57979d7f5b68dbdfb6b7b64c314349a0a960ae9e
Logan Wallace <mrwallacelk@gmail.com>
2022-07-05 13:51:29 -0700

    bundle

---
+++ src/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'nexsys.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: `bundle.min.js`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        options: {
+          cacheDirectory: true,
+          presets: [
+            '@babel/preset-env',
+          ],
+        },
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mangle: false,
+          keep_fnames: true,
+          toplevel: false,
+          keep_classnames: true,
+        },
+      }),
+    ],
+  },
+};
