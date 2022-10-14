a9a297bd9772e3637a03b68f9c502dd828ac598f
Logan Wallace <mrwallacelk@gmail.com>
2022-10-14 16:02:29 -0700

    yes

--- src/components/Dndkit.js
+++ src/components/Dndkit.js
@@ -12,19 +12,32 @@ import {
   SortableContext,
   sortableKeyboardCoordinates,
   horizontalListSortingStrategy,
+  rectSortingStrategy
 } from '@dnd-kit/sortable';
 
 import {SortableItem} from './SortableItem';
+import { Droppable } from './Droppable';
 
 function Dndkit() {
-  const [items, setItems] = useState([1, 2, 3]);
+  const [items, setItems] = useState([1, 2, 3, 4, 5, 6]);
+
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
       coordinateGetter: sortableKeyboardCoordinates,
     })
   );
-
+  const containerStyle = {
+    background: "red",
+    padding: 10,
+    margin: 10,
+    width: 200,
+    height: 300,
+    flex: 1,
+    flexDirection: "row",
+    display: "flex",
+    'flex-wrap': 'wrap'
+  };
   return (
     <DndContext 
       sensors={sensors}
@@ -33,10 +46,26 @@ function Dndkit() {
     >
       <SortableContext 
         items={items}
-        strategy={horizontalListSortingStrategy}
+        strategy={rectSortingStrategy}
       >
-        {items.map(id => <SortableItem key={id} id={id} />)}
+        <Droppable id={"one"}>
+        <div style={containerStyle}>
+                  {items.map(id => <SortableItem key={id} id={id} />)}
+                  </div>
+                  </Droppable>
       </SortableContext>
+
+      <SortableContext 
+      items={[]}
+        strategy={rectSortingStrategy}
+      >
+        <Droppable id={'two'}>
+        <div style={containerStyle}>
+
+                  </div>
+                  </Droppable>
+      </SortableContext>
+
     </DndContext>
   );
   
--- src/components/SortableItem.js
+++ src/components/SortableItem.js
@@ -9,16 +9,33 @@ export function SortableItem(props) {
     setNodeRef,
     transform,
     transition,
+    isDragging
   } = useSortable({id: props.id});
   
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
+    userSelect: "none",
+  padding: 8 * 2,
+  margin: `0 ${8}px 0 0`,
   };
   
   return (
     <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      {'Hello'}
+      {'Hello'} {props.id}
     </div>
   );
 }
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: 8 * 2,
+  margin: `0 ${8}px 0 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
