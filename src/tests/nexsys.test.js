d97dd5826f149dacc185d076669620af75c670b2
Log-Wall <mrwallacelk@gmail.com>
2022-12-26 08:38:18 -0800

    page

--- src/tests/nexsys.test.js
+++ src/tests/nexsys.test.js
@@ -1,26 +1,29 @@
-import nexSys from "../nexSys";
-import { eventStream } from "../base/eventStream"
+import { eventStream } from "../base/eventStream";
 
-describe('basic eventStream functionality', () => {
-  test('eventStream loaded', () => {
-    expect(eventStream).toBeDefined()
+describe("basic eventStream functionality", () => {
+  test("eventStream loaded", () => {
+    expect(eventStream).toBeDefined();
   });
 
-  test('add event to eventStream',() => {
-    let testEvent = ()=>{console.log('hello world')};
-    eventStream.registerEvent('testEvent', testEvent)
-    expect(eventStream.stream).toHaveProperty('testEvent');
+  test("add event to eventStream", () => {
+    let testEvent = () => {
+      console.log("hello world");
+    };
+    eventStream.registerEvent("testEvent", testEvent);
+    expect(eventStream.stream).toHaveProperty("testEvent");
   });
 
-  test('remove event by name from eventStream', () => {
-    eventStream.removeListener('testEvent', 'testEvent');
-    expect(eventStream.stream['testEvent']).toHaveLength(0);
+  test("remove event by name from eventStream", () => {
+    eventStream.removeListener("testEvent", "testEvent");
+    expect(eventStream.stream["testEvent"]).toHaveLength(0);
   });
 
-  test('remove event by object from eventStream', () => {
-    let testEvent = ()=>{console.log('hello world')};
-    eventStream.registerEvent('testEvent', testEvent)
-    eventStream.removeListener('testEvent', testEvent);
-    expect(eventStream.stream['testEvent']).toHaveLength(0);
+  test("remove event by object from eventStream", () => {
+    let testEvent = () => {
+      console.log("hello world");
+    };
+    eventStream.registerEvent("testEvent", testEvent);
+    eventStream.removeListener("testEvent", testEvent);
+    expect(eventStream.stream["testEvent"]).toHaveLength(0);
   });
-})
+});
