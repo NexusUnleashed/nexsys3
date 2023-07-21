ce383e8baf1b01acf44cd302d7e4977fce4f09b1
bryan-hunter <bryan.hunter@asurion.com>
2023-07-21 14:14:29 -0500

    add all classes

--- src/nexsys.js
+++ src/nexsys.js
@@ -19,6 +19,7 @@
 */
 /* global globalThis */
 import { affs } from "./base/affs/affs";
+import { Aff, AffCountable, AffTimed, AffDef } from "./base/affs/Aff";
 import {
   affPrioSwap,
   getCurrentAffs,
@@ -28,8 +29,10 @@ import {
   setPrioArrays,
 } from "./base/affs/affService";
 import { affTable } from "./base/affs/affTable";
+import { Balance, BalExtended } from "./base/balances/Balance";
 import { bals } from "./base/balances/balances";
 import { haveABal, haveBal, haveBals } from "./base/balances/balanceService";
+import { Cache } from "./base/cache/Cache";
 import { caches } from "./base/cache/caches";
 import { getCacheOutputs } from "./base/cache/cacheService";
 import { cacheTable } from "./base/cache/cacheTable";
@@ -40,6 +43,7 @@ import Timer from "./base/classes/Timer";
 import Trackable from "./base/classes/Trackable";
 import { prompt } from "./base/clientOverrides/prompt";
 import { cures } from "./base/cures/cures";
+import { Def, DefServerside } from "./base/defs/Def";
 import { defs } from "./base/defs/defs";
 import {
   defoff,
@@ -110,11 +114,20 @@ const nexSys = {
   sysLoggingToggle: sysLoggingToggle,
 
   classes: {
-    Countable: Countable,
-    Priority: Priority,
-    Priorityqueue: Priorityqueue,
-    Timer: Timer,
-    Trackable: Trackable,
+    Countable,
+    Priority,
+    Priorityqueue,
+    Timer,
+    Trackable,
+    Aff,
+    AffCountable,
+    AffTimed,
+    AffDef,
+    Balance,
+    BalExtended,
+    Cache,
+    Def,
+    DefServerside,
   },
 
   cures: cures,
