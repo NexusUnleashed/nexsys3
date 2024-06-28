15917085cbb8875691d8143130a86d362b54dff3
Logan Wallace <mrwallacelk@gmail.com>
2024-06-28 15:08:38 -0700

    eventGmcpBalances function changed to not fire continually

--- src/base/balances/balanceEvents.js
+++ src/base/balances/balanceEvents.js
@@ -1,30 +1,28 @@
 /* global eventStream */
-import {haveBal} from './balanceService';
+import { haveBal } from "./balanceService";
 
-let eventGmcpBalances = function(vitals) {
-  if (vitals.bal === '1') {
-      // CUSTOM event for recovering eqbal tracking
-      if (!haveBal('balance') && haveBal('equilibrium')) {
-          eventStream.raiseEvent('eqBalRecoveredEvent');
-      }
-      
-      eventStream.raiseEvent('balanceGotBalEvent');
-  }
-  else {
-      eventStream.raiseEvent('balanceLostBalEvent');
-  }
-  
-  if(vitals.eq === '1') {
-      // CUSTOM event for recovering eqbal tracking
-      if (!haveBal('equilibrium') && haveBal('balance')) {
-          eventStream.raiseEvent('eqBalRecoveredEvent');
-      }
+let eventGmcpBalances = function (vitals) {
+  if (vitals.bal === "1" && !haveBal("balance")) {
+    // CUSTOM event for recovering eqbal tracking
+    if (!haveBal("balance") && haveBal("equilibrium")) {
+      eventStream.raiseEvent("eqBalRecoveredEvent");
+    }
 
-      eventStream.raiseEvent('equilibriumGotBalEvent');
+    eventStream.raiseEvent("balanceGotBalEvent");
+  } else if (vitals.bal === "0" && haveBal("balance")) {
+    eventStream.raiseEvent("balanceLostBalEvent");
   }
-  else {
-      eventStream.raiseEvent('equilibriumLostBalEvent');
+
+  if (vitals.eq === "1" && !haveBal("equilibrium")) {
+    // CUSTOM event for recovering eqbal tracking
+    if (!haveBal("equilibrium") && haveBal("balance")) {
+      eventStream.raiseEvent("eqBalRecoveredEvent");
+    }
+
+    eventStream.raiseEvent("equilibriumGotBalEvent");
+  } else if (vitals.eq === "0" && haveBal("equilibrium")) {
+    eventStream.raiseEvent("equilibriumLostBalEvent");
   }
 };
 
-eventStream.registerEvent('Char.Vitals', eventGmcpBalances);
+eventStream.registerEvent("Char.Vitals", eventGmcpBalances);
