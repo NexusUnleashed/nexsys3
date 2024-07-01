25f76dbe723cee14f66a45714ba621aef0f77e8c
Logan Wallace <mrwallacelk@gmail.com>
2024-07-01 16:00:46 -0700

    balances

--- src/base/balances/balanceEvents.js
+++ src/base/balances/balanceEvents.js
@@ -3,23 +3,22 @@ import { haveBal } from "./balanceService";
 
 let eventGmcpBalances = function (vitals) {
   if (vitals.bal === "1" && !haveBal("balance")) {
+    eventStream.raiseEvent("balanceGotBalEvent");
     // CUSTOM event for recovering eqbal tracking
     if (!haveBal("balance") && haveBal("equilibrium")) {
       eventStream.raiseEvent("eqBalRecoveredEvent");
     }
-
-    eventStream.raiseEvent("balanceGotBalEvent");
   } else if (vitals.bal === "0" && haveBal("balance")) {
     eventStream.raiseEvent("balanceLostBalEvent");
   }
 
   if (vitals.eq === "1" && !haveBal("equilibrium")) {
+    eventStream.raiseEvent("equilibriumGotBalEvent");
+
     // CUSTOM event for recovering eqbal tracking
     if (!haveBal("equilibrium") && haveBal("balance")) {
       eventStream.raiseEvent("eqBalRecoveredEvent");
     }
-
-    eventStream.raiseEvent("equilibriumGotBalEvent");
   } else if (vitals.eq === "0" && haveBal("equilibrium")) {
     eventStream.raiseEvent("equilibriumLostBalEvent");
   }
