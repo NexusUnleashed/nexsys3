7ab0a72f4c0114ca916f1537a71a58cb44ce976e
Logan Wallace <mrwallacelk@gmail.com>
2024-10-22 14:25:35 -0700

    1.9.5

--- src/nexsys.js
+++ src/nexsys.js
@@ -126,7 +126,7 @@ import { startup } from "./base/utilities/startup";
 import NexDialog from "./components/NexDialog";
 
 const nexSys = {
-  version: "1.9.4",
+  version: "1.9.5",
   evt: new EventTarget(),
   component: NexDialog,
 
