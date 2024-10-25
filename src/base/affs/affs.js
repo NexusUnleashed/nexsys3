664a4c509d9631624a168ad6aa4a34e04b61c86f
Logan Wallace <mrwallacelk@gmail.com>
2024-10-25 15:54:03 -0700

    1.9.6

--- src/base/affs/affs.js
+++ src/base/affs/affs.js
@@ -33,8 +33,11 @@ for (let i = 0; i < affTable.list.length; i++) {
       ].includes(affname)
     ) {
       for (let i = 1; i < count.max + 1; i++) {
-        const prio = i === 1 ? 0 : prio;
-        affs[`${affname}${i}`] = new Aff(`${affname}${i}`, prio, uncurable);
+        if (i === 1) {
+          affs[`${affname}${i}`] = new Aff(`${affname}${i}`, 0, uncurable);
+        } else {
+          affs[`${affname}${i}`] = new Aff(`${affname}${i}`, prio, uncurable);
+        }
       }
     }
   } else if (timed) {
