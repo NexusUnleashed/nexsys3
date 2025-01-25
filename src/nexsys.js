4fbc3f4d498ead2d4799d4c6008212f7dcca69f0
Log-Wall <mrwallacelk@gmail.com>
2025-01-25 11:45:39 -0800

    2.0.8

--- src/nexsys.js
+++ src/nexsys.js
@@ -126,7 +126,7 @@ import { startup } from "./base/utilities/startup";
 import NexDialog from "./components/NexDialog";
 
 const nexSys = {
-  version: "2.0.7",
+  version: "2.0.8",
   evt: new EventTarget(),
   component: NexDialog,
 
