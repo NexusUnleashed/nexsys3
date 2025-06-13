cff9e653a8d8882909f569827177cf28277b5420
Khaseem <mrwallacelk@gmail.com>
2025-06-13 14:59:45 -0700

    2.1.7

--- src/nexsys.js
+++ src/nexsys.js
@@ -128,7 +128,7 @@ import { Limb } from "./base/selfLimbcounter/Limb";
 import { limbs } from "./base/selfLimbcounter/limbs";
 
 const nexSys = {
-  version: "2.1.6",
+  version: "2.1.7",
   evt: new EventTarget(),
   component: NexDialog,
 
